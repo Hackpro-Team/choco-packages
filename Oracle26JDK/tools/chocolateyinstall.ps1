7e7b4c61bfc10c0b598f9a46bce106204b685b37
Hackpro <hackpro.ems@gmail.com>
2026-07-10 14:58:04 -0500

    Switch from MSI to EXE installer to fix content-type validation (#4)
    Co-authored-by: Edwin Mantilla Santamaria <emantilla@deloitte.com>

--- Oracle26JDK/tools/chocolateyinstall.ps1
+++ Oracle26JDK/tools/chocolateyinstall.ps1
@@ -1,10 +1,10 @@
 $packageArgs = @{
   PackageName    = $env:ChocolateyPackageName
-  Url64bit       = 'https://download.oracle.com/java/26/latest/jdk-26_windows-x64_bin.msi'
-  Checksum64     = 'b1f1dcfccb6a2ad64627aeb26b84dffa3bbeaae1509367858917b321f8c8ae07'
+  Url64bit       = 'https://download.oracle.com/java/26/latest/jdk-26_windows-x64_bin.exe'
+  Checksum64     = '64e99c9fe382627d3b4b40ca737dfc24c4571af8f07db561c686b741f1eeccde'
   ChecksumType64 = 'sha256'
-  fileType       = 'msi'
-  silentArgs     = "INSTALLLEVEL=3 /quiet"
+  fileType       = 'exe'
+  silentArgs     = '/s'
 }
 
 Install-ChocolateyPackage @packageArgs
